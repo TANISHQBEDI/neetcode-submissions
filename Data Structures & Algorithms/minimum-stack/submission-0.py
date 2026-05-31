b3d4d8589e22be4bec10db0bff1a6603e50cb91e
Tanishq Bedi <66412510+TANISHQBEDI@users.noreply.github.com>
2026-05-31 14:07:31 +1000

    Add: minimum-stack - submission-0

---
+++ Data Structures & Algorithms/minimum-stack/submission-0.py
@@ -0,0 +1,20 @@
+class MinStack:
+
+    def __init__(self):
+        self.stack = []
+
+    def push(self, val: int) -> None:
+        if self.stack:
+            curr_min = min(self.stack[-1][1], val)
+        else:
+            curr_min = val
+        self.stack.append((val, curr_min))
+
+    def pop(self) -> None:
+        self.stack.pop()
+
+    def top(self) -> int:
+        return self.stack[-1][0]
+
+    def getMin(self) -> int:
+        return self.stack[-1][1]
