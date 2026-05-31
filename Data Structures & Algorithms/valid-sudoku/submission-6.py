3cae908c00903f264e38dd8fdf84e8c7cfd67d45
Tanishq Bedi <66412510+TANISHQBEDI@users.noreply.github.com>
2026-05-31 14:01:39 +1000

    Add: valid-sudoku - submission-6

---
+++ Data Structures & Algorithms/valid-sudoku/submission-6.py
@@ -0,0 +1,17 @@
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        rows = defaultdict(set)
+        cols = defaultdict(set)
+        sub_box = defaultdict(set)
+
+        for i in range(9):
+            for j in range(9):
+                curr = board[i][j]
+                if curr == '.': continue
+                if curr in rows[i] or curr in cols[j] or curr in sub_box[(i // 3, j // 3)]:
+                    return False
+                rows[i].add(curr)
+                cols[j].add(curr)
+                sub_box[(i//3, j//3)].add(curr)
+
+        return True
